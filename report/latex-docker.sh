314587f99832f8fd80b68cbc7093ae1d16ad3e15
Denis Strizhkin <strdenis02@gmail.com>
2026-01-14 15:38:19 +0300

    Add readme

--- report/latex-docker.sh
+++ report/latex-docker.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env sh
+if ! podman image exists texlive; then
+    echo "Image 'texlive' not found. Building from https://github.com/denisstrizhkin/texlive-container.git ..."
+    podman build -t texlive https://github.com/denisstrizhkin/texlive-container.git
+fi
+
 podman run --rm -it -v "$PWD:/work:Z" -w /work texlive "$@"
--- run_all.sh
+++ run_all.sh
@@ -9,12 +9,13 @@ mkdir -p report/images
 gnuplot plot_graphs.gp
 
 echo "3. Compiling LaTeX report..."
-# Use the local docker script if available, or call podman directly
-if [ -f "report/latex-docker.sh" ]; then
-    # The script uses $PWD, so we run it from the root but target the report folder
-    podman run --rm -v "$(pwd):/work:Z" -w /work/report texlive pdflatex report.tex
-else
-    echo "Warning: report/latex-docker.sh not found, skipping PDF compilation."
+# Build texlive image if not present
+if ! podman image exists texlive; then
+    echo "Image 'texlive' not found. Building from https://github.com/denisstrizhkin/texlive-container.git ..."
+    podman build -t texlive https://github.com/denisstrizhkin/texlive-container.git
 fi
+
+# Run the compilation
+podman run --rm -v "$(pwd):/work:Z" -w /work/report texlive pdflatex report.tex
 
 echo "All done! Report generated at report/report.pdf"
